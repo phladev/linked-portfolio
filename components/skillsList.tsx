270595044c57067bf223c0d29d101ff21fc8224b
phladev <phladev05@gmail.com>
2024-11-14 19:40:00 -0300

    feat: add abilities page

---
+++ components/skillsList.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent } from '@/components/ui/card'
+
+interface SkillListProps {
+  skills: string[]
+}
+
+export default function SkillList({ skills }: SkillListProps) {
+  return (
+    <Card className="h-[320px] bg-gray-800 border-gray-700 hover:border-[#7e22ce] transition-colors md:h-[400px]">
+      <CardContent className="mt-4">
+        <ul className="list-disc list-inside text-gray-300 space-y-1">
+          {skills.map((skill, index) => (
+            <li key={index} className="text-sm md:text-lg">
+              {skill}
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  )
+}
